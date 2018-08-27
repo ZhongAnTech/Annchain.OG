ecb551ed5c7b4677dedf1cb14765ce7857a2b56a
jenson <metab@qq.com>
2018-08-27 19:39:43 +0800

    p2p init

--- node/node.go
+++ node/node.go
@@ -4,23 +4,24 @@ import (
 	"github.com/annchain/OG/og"
 	"github.com/annchain/OG/p2p"
 	"github.com/annchain/OG/rpc"
-	"github.com/spf13/viper"
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 )
 
 // Node is the basic entrypoint for all modules to start.
 type Node struct {
-	og *og.Og
-	p2pServer *p2p.P2PServer
+	og        *og.Og
+	p2pServer *p2p.Server
 	rpcServer *rpc.RpcServer
 }
 
 func NewNode() *Node {
 	n := new(Node)
-	if viper.GetBool("p2p.enabled"){
-		n.p2pServer = p2p.NewP2PServer(viper.GetString("p2p.port"))
+	if viper.GetBool("p2p.enabled") {
+		privKey := getNodePrivKey()
+		n.p2pServer = NewP2PServer(privKey)
 	}
-	if viper.GetBool("rpc.enabled"){
+	if viper.GetBool("rpc.enabled") {
 		n.rpcServer = rpc.NewRpcServer(viper.GetString("rpc.port"))
 	}
 	n.og = new(og.Og)
@@ -31,21 +32,21 @@ func (n *Node) Start() {
 	// start myself first and then provide service and do p2p
 	n.og.Start()
 
-	if n.p2pServer != nil{
+	if n.p2pServer != nil {
 		logrus.Info("Starting p2p server")
 		n.p2pServer.Start()
 	}
-	if n.rpcServer != nil{
+	if n.rpcServer != nil {
 		logrus.Info("Starting rpc server")
 		n.rpcServer.Start()
 	}
 	logrus.Info("Node Started")
 }
 func (n *Node) Stop() {
-	if n.rpcServer != nil{
+	if n.rpcServer != nil {
 		n.rpcServer.Stop()
 	}
-	if n.p2pServer != nil{
+	if n.p2pServer != nil {
 		n.p2pServer.Stop()
 	}
 	n.og.Stop()

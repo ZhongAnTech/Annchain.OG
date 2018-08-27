69ebc5afcf446d95a8bc18d89ffa98c0e2b90ece
latifrons <latifrons88@gmail.com>
2018-08-27 16:19:01 +0800

    Tx interface type

--- types/tx_inferface.go
+++ types/tx_inferface.go
@@ -3,16 +3,28 @@ package types
 //go:generate msgp
 //msgp:tuple TxBase
 
+const (
+	TxTypeNormal    uint = iota
+	TxTypeSequencer
+)
+
 type Txi interface {
 	// Hash returns a tx hash
 	Hash() Hash
 
 	// Parents returns the hash of txs that it directly proves.
 	Parents() []Hash
+	GetType() int
 }
 type TxBase struct {
 	Type          int    `msgp:"type"`
 	ParentsHash   []Hash `msgp:"parentHash"`
 	SequenceNonce uint64 `msgp:"sequenceNonce"`
 	Height        uint64 `msgp:"height"`
+
+
+}
+
+func (t *TxBase) GetType() int{
+	return t.Type
 }

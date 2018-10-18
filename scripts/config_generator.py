291583c66af13bb3608d91f77dd8b98abf0d718b
latifrons <latifrons88@gmail.com>
2018-10-18 20:34:02 +0800

    script updates for generating new config.

--- scripts/config_generator.py
+++ scripts/config_generator.py
@@ -2,7 +2,6 @@ import sys
 
 import toml
 
-parallel_tx = 1
 static_node = 'a4d8435e9923d6cc950ff47df5f8fef469b1acfa255cf9b5daab53abdca3d76403bda33ebedd1336d526ffc510c9420c7df0731ee37ea6b28780ca6011fe741e'
 
 
@@ -14,16 +13,18 @@ def generate_config(id, seq_enabled):
 
     d['rpc']['port'] = port_add + 0
     d['p2p']['port'] = port_add + 1
+    d['p2p']['bootstrap_node'] = id == 0
+    d['p2p']['static_nodes'] = "enode://%s@127.0.0.1:%d" % (static_node, 8001)
+
     d['websocket']['port'] = port_add + 2
     d['profiling']['port'] = port_add + 3
     d['leveldb']['path'] = 'datadir_%02d' % (id)
 
-    d['auto_tx']['enabled'] = True
-    # d['auto_tx']['count'] = parallel_tx
-    d['auto_sequencer']['enabled'] = seq_enabled
-    d['debug']['node_id'] = id
+    d['auto_client']['sequencer']['enabled'] = seq_enabled
+    d['auto_client']['tx']['enabled'] = True
+    d['auto_client']['tx']['account_ids'] = [id,]
 
-    d['p2p']['static_nodes'] = "enode://%s@127.0.0.1:%d" % (static_node, 8001)
+    d['debug']['node_id'] = id
 
     with open('config_%02d.toml' % (id), 'w') as f:
         toml.dump(d, f)
@@ -32,8 +33,8 @@ def generate_config(id, seq_enabled):
 
 
 if __name__ == '__main__':
-    total = int(sys.argv[1])
-    enable_sequencer = sys.argv[2] == '1'
+    total = len(sys.argv) <= 1 and 10 or sys.argv[1]
+    enable_sequencer = len(sys.argv) <= 2 or sys.argv[2] == '1'
 
     print('Total nodes: %d. Sequencer: %s' % (total, enable_sequencer))
 

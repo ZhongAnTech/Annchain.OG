46c9893f3fcb2da599c537b9fb128630e1c26f3e
heeeeeng <boh_zhu@163.com>
2018-10-22 11:40:52 +0800

    move timeout to config

--- types/common.go
+++ types/common.go
@@ -1,7 +1,27 @@
 package types
 
+import (
+	"time"
+)
+
+
 func panicIfError(err error) {
 	if err != nil {
 		panic(err)
+	}
+}
+
+func ChannelHandler(timer *time.Timer, receiver chan interface{}, data interface{}) {
+	for {
+		if !timer.Stop(){
+			<- timer.C
+		}
+		timer.Reset(time.Second * 10)
+		select {
+		case <-timer.C:
+			// log.Warn("timeout on channel writing: batch confirmed")
+		case receiver <- data:
+			break 
+		}
 	}
 }

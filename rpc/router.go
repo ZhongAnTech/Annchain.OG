7f947697a0205423af459cda5689c82c7ed4819e
metabd <met_a@foxmail.com>
2018-12-06 16:55:55 +0800

    add monitor

--- rpc/router.go
+++ rpc/router.go
@@ -47,6 +47,7 @@ func (rpc *RpcController) Newrouter() *gin.Engine {
 
 	router.GET("debug", rpc.Debug)
 	router.GET("tps", rpc.Tps)
+	router.GET("monitor",rpc.Monitor)
 	router.GET("sync_status", rpc.SyncStatus)
 	router.GET("performance", rpc.Performance)
 	return router
@@ -68,6 +69,7 @@ func (rpc *RpcController) writeListOfEndpoints(c *gin.Context) {
 		"sync_status":   "",
 		"performance":   "",
 		"tps":			 "",
+		"monitor":"",
 		// broadcast API
 		"new_transaction": "tx",
 		"auto_tx":         "interval_ms",

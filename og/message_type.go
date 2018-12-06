e1f5bc298e9676cb59474dcd7ad83cc1a435a354
latifrons <latifrons88@gmail.com>
2018-12-06 14:38:59 +0800

    MustSetBytes in Hash now takes padding option to decide which direction padding should be added if length < 32. Default is PaddingNone(panic if != 32)

--- og/message_type.go
+++ og/message_type.go
@@ -95,7 +95,7 @@ func (m *P2PMessage) calculateHash() {
 	h := sha256.New()
 	h.Write(data)
 	sum := h.Sum(nil)
-	m.hash.MustSetBytes(sum)
+	m.hash.MustSetBytes(sum, types.PaddingNone)
 	return
 }
 

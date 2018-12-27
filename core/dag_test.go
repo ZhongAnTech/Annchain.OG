1951cf365750f7698ceeaa9f0180e4fe59f3c1f1
heeeeeng <boh_zhu@163.com>
2018-12-27 18:21:15 +0800

    let ovm.Call and ovm.Create able to know if its caller is tx or contract, this is for value transfer and nonce setting in vm; update process tx unit test

--- core/dag_test.go
+++ core/dag_test.go
@@ -194,6 +194,7 @@ func TestDagProcess(t *testing.T) {
 	var err error
 
 	dag, _, finish := newTestDag(t, "TestDagProcess")
+	stdb := dag.StateDatabase()
 	defer finish()
 
 	pk, _ := crypto.PrivateKeyFromString(testPkSecp0)
@@ -201,7 +202,7 @@ func TestDagProcess(t *testing.T) {
 
 	// evm contract bytecode, for source code detail please check:
 	// github.com/annchain/OG/vm/vm_test/contracts/setter.sol
-	contractCode := "6060604052341561000f57600080fd5b600a6000819055506101c9806100266000396000f300606060405260043610610057576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff16806360fe47b11461005c578063c605f76c1461007f578063e5aa3d581461010d575b600080fd5b341561006757600080fd5b61007d6004808035906020019091905050610136565b005b341561008a57600080fd5b610092610140565b6040518080602001828103825283818151815260200191508051906020019080838360005b838110156100d25780820151818401526020810190506100b7565b50505050905090810190601f1680156100ff5780820380516001836020036101000a031916815260200191505b509250505060405180910390f35b341561011857600080fd5b610120610183565b6040518082815260200191505060405180910390f35b8060008190555050565b610148610189565b6040805190810160405280600a81526020017f68656c6c6f576f726c6400000000000000000000000000000000000000000000815250905090565b60005481565b6020604051908101604052806000815250905600a165627a7a72305820746cc41ab9d8793025829e7c5703d2685ff43efe9c4287079ee7d55868b4dc5c0029"
+	contractCode := "6060604052341561000f57600080fd5b600a60008190555060006001819055506102078061002e6000396000f300606060405260043610610062576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff1680631c0f72e11461006b57806360fe47b114610094578063c605f76c146100b7578063e5aa3d5814610145575b34600181905550005b341561007657600080fd5b61007e61016e565b6040518082815260200191505060405180910390f35b341561009f57600080fd5b6100b56004808035906020019091905050610174565b005b34156100c257600080fd5b6100ca61017e565b6040518080602001828103825283818151815260200191508051906020019080838360005b8381101561010a5780820151818401526020810190506100ef565b50505050905090810190601f1680156101375780820380516001836020036101000a031916815260200191505b509250505060405180910390f35b341561015057600080fd5b6101586101c1565b6040518082815260200191505060405180910390f35b60015481565b8060008190555050565b6101866101c7565b6040805190810160405280600a81526020017f68656c6c6f576f726c6400000000000000000000000000000000000000000000815250905090565b60005481565b6020604051908101604052806000815250905600a165627a7a723058208e1bdbeee227900e60082cfcc0e44d400385e8811ae77ac6d7f3b72f630f04170029"
 
 	createTx := &types.Tx{}
 	createTx.From = addr
@@ -214,13 +215,8 @@ func TestDagProcess(t *testing.T) {
 	if err != nil {
 		t.Fatalf("error during contract creation: %v", err)
 	}
-	// nonce, nerr := dag.GetLatestNonce(addr)
-	// if nerr != nil && nerr != types.ErrNonceNotExist {
-	// 	t.Fatalf("get latest nonce from dag error: %v", nerr)
-	// }
 	contractAddr := crypto.CreateAddress(addr, uint64(0))
 
-	stdb := dag.StateDatabase()
 	cObj := stdb.GetStateObject(contractAddr)
 	if cObj == nil {
 		t.Fatalf("contract object not initiated in statedb")
@@ -232,7 +228,6 @@ func TestDagProcess(t *testing.T) {
 
 	// get i from setter contract
 	calldata := "e5aa3d58"
-
 	callTx := &types.Tx{}
 	callTx.From = addr
 	callTx.Value = math.NewBigInt(0)
@@ -242,6 +237,46 @@ func TestDagProcess(t *testing.T) {
 	if err != nil {
 		t.Fatalf("error during contract calling: %v", err)
 	}
+	targetstr := "000000000000000000000000000000000000000000000000000000000000000a"
+	retstr := fmt.Sprintf("%x", ret)
+	if retstr != targetstr {
+		t.Fatalf("the [i] in contract is not 10, should be %s, get %s", targetstr, retstr)
+	}
 
-	fmt.Println(ret)
+	// set i to be 100
+	setdata := "60fe47b10000000000000000000000000000000000000000000000000000000000000064"
+	setTx := &types.Tx{}
+	setTx.From = addr
+	setTx.Value = math.NewBigInt(0)
+	setTx.To = contractAddr
+	setTx.Data, _ = hex.DecodeString(setdata)
+	ret, err = dag.ProcessTransaction(setTx)
+	if err != nil {
+		t.Fatalf("error during contract setting: %v", err)
+	}
+	// get i and check if it is changed
+	ret, err = dag.ProcessTransaction(callTx)
+	if err != nil {
+		t.Fatalf("error during contract calling: %v", err)
+	}
+	targetstr = "0000000000000000000000000000000000000000000000000000000000000064"
+	retstr = fmt.Sprintf("%x", ret)
+	if retstr != targetstr {
+		t.Fatalf("the [i] in contract is not 100, should be %s, get %s", targetstr, retstr)
+	}
+
+	// pay a 10 bill to contract
+	transferValue := int64(10)
+	payTx := &types.Tx{}
+	payTx.From = addr
+	payTx.Value = math.NewBigInt(transferValue)
+	payTx.To = contractAddr
+	ret, err = dag.ProcessTransaction(payTx)
+	if err != nil {
+		t.Fatalf("error during contract setting: %v", err)
+	}
+	blc := stdb.GetBalance(contractAddr)
+	if blc.GetInt64() != transferValue {
+		t.Fatalf("the value is not tranferred to contract, should be: %d, get: %d", transferValue, blc.GetInt64())
+	}
 }

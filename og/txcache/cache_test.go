7c800222245cbb6e9adbec313bca4f4892358ffb
jenson <metab@foxmail.com>
2018-12-28 11:04:21 +0800

    use ordered txcache

--- og/txcache/cache_test.go
+++ og/txcache/cache_test.go
@@ -21,7 +21,10 @@ func init_log() {
 
 func newTestTxcache()*TxCache {
 	logrus.SetLevel(logrus.InfoLevel)
-	c := NewTxCache(100000,3)
+	invalidTx := func (h types.Hash) bool {
+		return true
+	}
+	c := NewTxCache(100000,3,invalidTx)
 	for i:=0; i<40000;i++ {
 		var tx types.Txi
 		if i%40==0 {
@@ -29,7 +32,10 @@ func newTestTxcache()*TxCache {
 		}else {
 			tx = types.RandomTx()
 		}
-		c.EnQueue(tx)
+		err := c.EnQueue(tx)
+		if err!=nil {
+			panic(err)
+		}
 	}
 	logrus.SetLevel(logrus.DebugLevel)
 	return c
@@ -47,7 +53,7 @@ func TestTxCache_Get(t *testing.T) {
 	 if c.Has(tx2.GetTxHash()) {
 	 	t.Fatal("tx is not in cache")
 	 }
-	 hash := c.order[5]
+	 hash := c.GetHashOrder()[5]
 	 t.Log("hash",hash)
 	 if tx := c.Get(hash) ;tx!=nil {
 	 	t.Log(tx.String())
@@ -62,6 +68,7 @@ func TestTxCache_Get(t *testing.T) {
 	c.Remove(hash)
 	t.Log("remove tx",time.Now().Sub(start).String())
 	 time.Sleep(time.Second*4)
+	c.Refresh()
 	if c.cache.Len()!=0 {
 		t.Fatalf("requied 0 , got %d",c.Len())
 	}
@@ -70,48 +77,45 @@ func TestTxCache_Get(t *testing.T) {
 	 }
 	 c.Remove(tx1.GetTxHash())
 	 t.Log("len",c.Len())
-	 c.RemoveExpired()
+	c.RemoveExpiredAndInvalid(100)
 	t.Log("len",c.Len())
 }
 
 func TestTxCache_PopALl(t *testing.T) {
+
 	c := newTestTxcache()
-	start := time.Now()
-	txs:= c.PopALl()
-	t.Log("pop all used",time.Now().Sub(start).String())
-	for i,tx := range txs{
-		if i%1100==0 {
-			t.Log(tx.String())
-		}
+	fmt.Println(c.Len())
+	start  := time.Now()
+	for c.Len() !=0  {
+		c.DeQueue()     //10000 item 2.1s
+		//c.cache.Remove(hash) // 10000 item 6ms
 	}
 	if c.Len()!=0 {
 		t.Fatalf("requied 0 , got %d",c.Len())
 	}
+	t.Log("deQueue all used", time.Now().Sub(start).String())
 	c = newTestTxcache()
 	start  = time.Now()
 	hashes := c.GetHashOrder()
 	for _, hash := range hashes {
-		c.remove(hash)       //10000 item 2.1s
-		//c.cache.Remove(hash) // 10000 item 6ms
-	}
-	t.Log("remove all by hash cmp used", time.Now().Sub(start).String())
-
-	c = newTestTxcache()
-	start  = time.Now()
-	for c.Len() !=0  {
-		c.deQueue()     //10000 item 2.1s
-		//c.cache.Remove(hash) // 10000 item 6ms
-	}
-	t.Log("deQueue all used", time.Now().Sub(start).String())
-	c = newTestTxcache()
-	start  = time.Now()
-	for _, hash := range hashes {
 		//c.deQueue()     //10000 item 2.1s
 		c.cache.Remove(hash) // 10000 item 6ms
 	}
+	if c.Len()!=0 {
+		t.Fatalf("requied 0 , got %d",c.Len())
+	}
 	t.Log("directly remove  all used", time.Now().Sub(start).String())
+
+
+	c = newTestTxcache()
+	start  = time.Now()
+     c.cache.DeQueueBatch(39000)
+	c.cache.DeQueueBatch(3000)
+	if c.Len()!=0 {
+		t.Fatalf("requied 0 , got %d",c.Len())
+	}
+	t.Log("dequebatch remove  all used", time.Now().Sub(start).String())
    //result for 50000 items
-   //pop all used 100ms
    //remove all by hash cmp used 28.865s
    //deQueue all used 22ms
    //directly remove  all used 22ms
@@ -120,7 +124,6 @@ func TestTxCache_PopALl(t *testing.T) {
 func TestTxCache_EnQueue(t *testing.T) {
 	c:= newTestTxcache()
 	var wg sync.WaitGroup
-	wg.Add(1)
 	start := time.Now()
 	for i:=0; i<28000;i++{
 		tx:= types.RandomTx()
@@ -134,5 +137,60 @@ func TestTxCache_EnQueue(t *testing.T) {
 		}()
 	}
 	wg.Wait()
-	fmt.Println(c.cache.Len(),len(c.order),"used",time.Now().Sub(start))
+	fmt.Println(c.cache.Len(),"used",time.Now().Sub(start))
+}
+
+
+func TestTxCache_GetTop(t *testing.T) {
+	c:= newTestTxcache()
+	var wg sync.WaitGroup
+	start := time.Now()
+	for i:=0; i<28000;i++{
+		tx:= types.RandomTx()
+		wg.Add(1)
+		go func (int ){
+			//begin := time.Now()
+			c.EnQueue(tx)
+			//fmt.Println(time.Now().Sub(begin).String(),"enqueue")
+			if i%1000==0 {
+				fmt.Println("top,i", c.GetTop())
+			}else if i%1001 ==0 {
+				fmt.Println(c.DeQueueBatch(2))
+			}
+			wg.Done()
+			//fmt.Println(time.Now().Sub(begin).String(),"done")
+		}(i)
+	}
+	wg.Wait()
+	fmt.Println(c.cache.Len(),"used",time.Now().Sub(start))
+}
+
+
+func TestTxCache_DeQueueBatch(t *testing.T) {
+	c:= newTestTxcache()
+	c.DeQueueBatch(50000)
+	start := time.Now()
+	var txis [] *types.Tx
+	for i:=0; i<100;i++{
+		tx:= types.RandomTx()
+		txis = append(txis,tx)
+			//begin := time.Now()
+			c.EnQueue(tx)
+			//fmt.Println(time.Now().Sub(begin).String(),"enqueue")
+			if i%20==0 {
+				fmt.Println("top,i", c.GetTop())
+			}
+			//fmt.Println(time.Now().Sub(begin).String(),"done")
+		}
+
+	for i:=0; i<100;i++{
+		tx := c.DeQueue()
+		if tx!= txis[i] {
+			t.Fatal("diff ", tx ,txis[i])
+		}
+		if i%20==0 {
+			t.Log("same ", tx ,txis[i])
+		}
+	}
+	fmt.Println(c.cache.Len(),"used",time.Now().Sub(start))
 }

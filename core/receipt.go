238e74c9393eae5bbd83bc5ca4f135cc2a530206
heeeeeng <boh_zhu@163.com>
2019-01-04 11:39:37 +0800

    add GetReceipt function in RpcController; add receipt entry in dag

--- core/receipt.go
+++ core/receipt.go
@@ -1,6 +1,8 @@
 package core
 
 import (
+	"fmt"
+
 	"github.com/annchain/OG/types"
 )
 
@@ -29,6 +31,16 @@ func NewReceipt(hash types.Hash, status ReceiptStatus, pResult string, addr type
 		ProcessResult:   pResult,
 		ContractAddress: addr,
 	}
+}
+
+func (r *Receipt) ToJsonMap() map[string]string {
+	jm := make(map[string]string)
+	jm["hash"] = r.TxHash.Hex()
+	jm["status"] = fmt.Sprintf("%d", r.Status)
+	jm["result"] = r.ProcessResult
+	jm["contractAddress"] = r.ContractAddress.Hex()
+
+	return jm
 }
 
 //msgp:tuple ReceiptSet

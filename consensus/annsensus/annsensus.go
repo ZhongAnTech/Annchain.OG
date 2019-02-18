6d5f6edb7d1e4c10457f0a468fa69b2e1887cb80
heeeeeng <boh_zhu@163.com>
2019-02-18 14:48:13 +0800

    roughly implement annsensus

--- consensus/annsensus/annsensus.go
+++ consensus/annsensus/annsensus.go
@@ -2,16 +2,73 @@ package annsensus
 
 import (
 	"github.com/annchain/OG/types"
+	log "github.com/sirupsen/logrus"
 )
 
 type AnnSensus struct {
+	doCamp bool // the switch of whether annsensus should produce campaign.
+
+	receivers []chan types.Txi
 }
 
-// Commit takes a list of campaigns as input and record these
+func NewAnnSensus() *AnnSensus {
+	return &AnnSensus{
+		receivers: []chan types.Txi{},
+	}
+}
+
+func (as *AnnSensus) Start() {
+	log.Info("AnnSensus Start")
+	// TODO
+}
+
+func (as *AnnSensus) Stop() {
+	log.Info("AnnSensus Stop")
+	// TODO
+}
+
+// RegisterReceiver add a channel into AnnSensus.receivers, receivers are the
+// hooks that handle new txs.
+func (as *AnnSensus) RegisterReceiver(c chan types.Txi) {
+	as.receivers = append(as.receivers, c)
+}
+
+func (as *AnnSensus) CampaignOn() {
+	as.doCamp = true
+	go as.campaign()
+}
+
+func (as *AnnSensus) CampaignOff() {
+	as.doCamp = false
+}
+
+func (as *AnnSensus) campaign() {
+	// TODO
+	for {
+
+		if !as.doCamp {
+			return
+		}
+		// generate campaign.
+		camp := as.genCamp()
+
+		// send camp
+		for _, c := range as.receivers {
+			c <- camp
+		}
+
+	}
+}
+
+// genCamp calculate vrf and generate a campaign that contain this vrf info.
+func (as *AnnSensus) genCamp() *types.Campaign {
+	// TODO
+	return &types.Campaign{}
+}
+
+// commit takes a list of campaigns as input and record these
 // camps' information It checks if the number of camps reaches
 // the threshold. If so, start term changing flow.
-func (as *AnnSensus) Commit(camps []*types.Campaign) {
+func (as *AnnSensus) commit(camps []*types.Campaign) {
 
 }
-
-// func (as *AnnSensus)

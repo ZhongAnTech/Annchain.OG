7cc0e5b7e45cb7e68f2afc1572c86699f7ebb8df
jenson <metab@foxmail.com>
2019-02-21 15:14:40 +0800

    add msgp to kyber

--- common/crypto/dedis/kyber/v3/group.go
+++ common/crypto/dedis/kyber/v3/group.go
@@ -56,6 +56,7 @@ type Scalar interface {
 	// The endianess of the byte-slice is determined by the
 	// implementation.
 	SetBytes([]byte) Scalar
+
 }
 
 // Point represents an element of a public-key cryptographic Group.
@@ -110,6 +111,7 @@ type Point interface {
 	// Multiply point p by the scalar s.
 	// If p == nil, multiply with the standard base point Base().
 	Mul(s Scalar, p Point) Point
+
 }
 
 // AllowsVarTime allows callers to determine if a given kyber.Scalar
@@ -162,4 +164,5 @@ type Group interface {
 
 	PointLen() int // Max length of point in bytes
 	Point() Point  // Create new point
+
 }

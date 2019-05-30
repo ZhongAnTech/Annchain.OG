2a66c2d39d26ce0643210596f413410b7c9c316a
Uni <boh_zhu@163.com>
2019-05-30 18:53:13 +0800

    update to meet kubernetes requirements

--- DockerRun.sh
+++ DockerRun.sh
@@ -5,6 +5,6 @@ mkdir deployment/log
 docker run -p 8000:8000 -p 8001:8001 -p 8002:8002 -p 8003:8003 \
     -v "$PWD/deployment/datadir_1/:/opt/rw/datadir_1/" \
     -v "$PWD/deployment/log/:/opt/rw/log/" \
-    -v "$PWD/deployment/config.toml:/opt/rw/config.toml" \
-    -v "$PWD/deployment/genesis.json:/opt/rw/genesis.json" \
+    -v "$PWD/deployment/config.toml:/opt/config.toml" \
+    -v "$PWD/deployment/genesis.json:/opt/genesis.json" \
     og-alpine:latest

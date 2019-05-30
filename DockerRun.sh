916b3ffb9dc034b387157b1b9cb7d3fa0db43fab
Uni <boh_zhu@163.com>
2019-05-30 16:54:58 +0800

    Update DockerRun.sh

--- DockerRun.sh
+++ DockerRun.sh
@@ -5,6 +5,6 @@ mkdir deployment/log
 docker run -p 8000:8000 -p 8001:8001 -p 8002:8002 -p 8003:8003 \
     -v "$PWD/deployment/datadir_1/:/opt/datadir_1/" \
     -v "$PWD/deployment/log/:/opt/log/" \
-    -v "$PWD/config.toml:/opt/config.toml" \
-    -v "$PWD/genesis.json:/opt/genesis.json" \
+    -v "$PWD/deployment/config.toml:/opt/config.toml" \
+    -v "$PWD/deployment/genesis.json:/opt/genesis.json" \
     og-alpine:latest

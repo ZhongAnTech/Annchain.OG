50f59f32170d9e57fd9bad6e87271add17c79175
metabd <met_a@foxmail.com>
2019-05-30 11:13:38 +0800

    add henerate enode

---
+++ tests/createEnode/create_enode.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"github.com/annchain/OG/common/crypto"
+	"github.com/annchain/OG/p2p/onode"
+	"net"
+)
+
+func main() {
+	key, err := crypto.GenerateKey()
+	if err != nil {
+		panic(fmt.Sprintf("failed to generate ephemeral node key: %v", err))
+	}
+	data := crypto.FromECDSA(key)
+	fmt.Println("nodekey", hex.EncodeToString(data))
+	node := onode.NewV4(&key.PublicKey, net.ParseIP("0.0.0.0"), 8001, 8001)
+	fmt.Println(node)
+}
+

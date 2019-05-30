c06c0be02023c4cb00ef87b1279efb71d7a3a006
Uni <boh_zhu@163.com>
2019-05-30 15:59:20 +0800

    update deployment

---
+++ DockerRun.sh
@@ -0,0 +1,10 @@
+rm -r deployment/datadir_1
+mkdir deployment/datadir_1
+rm -r deployment/log
+mkdir deployment/log
+docker run -p 8000:8000 -p 8001:8001 -p 8002:8002 -p 8003:8003 \
+    -v "$PWD/deployment/datadir_1/:/opt/datadir_1/" \
+    -v "$PWD/deployment/log/:/opt/log/" \
+    -v "$PWD/config.toml:/opt/config.toml" \
+    -v "$PWD/genesis.json:/opt/genesis.json" \
+    og-alpine:latest
--- deploy.sh
+++
@@ -1,7 +0,0 @@
-docker build -t og-alpine:latest .
-mkdir build
-rm -r build/datadir_1
-mkdir build/datadir_1
-rm -r build/log
-mkdir build/log
-docker run -p 8000:8000 -p 8001:8001 -p 8002:8002 -p 8003:8003 -v "$PWD/build/datadir_1/:/opt/datadir_1/" -v "$PWD/build/log/:/opt/log/" og-alpine:latest

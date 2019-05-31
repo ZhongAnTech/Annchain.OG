74f7da40c9d3c209f197aa3a1c9662276e07b0fe
Uni <boh_zhu@163.com>
2019-05-31 10:55:47 +0800

    update go module

--- DockerRun.py
+++ DockerRun.py
@@ -2,17 +2,35 @@ import os
 import argparse
 import time
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-m", help="node mode, 'n' for normal node, 'p' for private node")
-parser.add_argument("-b", help="bootnode if this tag exists", action="store_true")
-parser.add_argument("-c", help="join consensus with a consensus filename")
+def deploy():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", help="node mode, 'n' for normal node, 'p' for private node")
+    parser.add_argument("-b", help="bootnode if this tag exists", action="store_true")
+    parser.add_argument("-c", help="join consensus with a consensus filename")
 
-args = parser.parse_args()
-print(args)
+    args = parser.parse_args()
+    print(args)
 
-time_format = "%Y-%m-%d_%H-%M-%S"
-cur_time = time.strftime(time_format, time.localtime())
-os.mkdir("Node " + cur_time)
+    time_format = "%Y-%m-%d_%H-%M-%S"
+    cur_time = time.strftime(time_format, time.localtime())
+    os.mkdir("/deployment/Node " + cur_time)
+
+    if args.m != 'n' and args.m != 'p':
+        print("invalid node mode, must be n or p")
+        return 
+
+    if args.m == 'n':
+        
+        return
+
+    if args.m == 'p':
+        pass
+
+     
+
+
+if __name__ == "__main__":
+    deploy()
 
 
 

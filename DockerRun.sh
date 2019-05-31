cf234b81f67c0599add66fac25540999ab72137b
Uni <boh_zhu@163.com>
2019-05-31 10:13:11 +0800

    Update DockerRun.sh

--- DockerRun.sh
+++ DockerRun.sh
@@ -1,7 +1,7 @@
-rm -r deployment/datadir_1
-mkdir deployment/datadir_1
-rm -r deployment/log
-mkdir deployment/log
+rm -r deployment/rw
+mkdir deployment/rw
+mkdir deployment/rw/datadir_1
+mkdir deployment/rw/log
 docker run -p 8000:8000 -p 8001:8001 -p 8002:8002 -p 8003:8003 \
     -v "$PWD/deployment/datadir_1/:/opt/rw/datadir_1/" \
     -v "$PWD/deployment/log/:/opt/rw/log/" \

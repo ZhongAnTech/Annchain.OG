d5b97b1138e38aba0de8be6f52d6f264b628ebe3
jenson <metab@foxmail.com>
2019-06-04 14:31:20 +0800

    fix secp256 noncgo test

--- common/crypto/non_cgo_test.go
+++ common/crypto/non_cgo_test.go
@@ -12,18 +12,88 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build noncgo
-
 package crypto
 
 import (
+	"crypto/ecdsa"
 	"encoding/hex"
 	"fmt"
 	"github.com/annchain/OG/types"
+	"github.com/btcsuite/btcd/btcec"
+	"math/big"
 	"testing"
 	"time"
 )
 
+type SignerSecp256k1Go struct {
+}
+
+func (s *SignerSecp256k1Go) GetCryptoType() CryptoType {
+	return CryptoTypeSecp256k1
+}
+
+// Sign calculates an ECDSA signature.
+//
+// This function is susceptible to chosen plaintext attacks that can leak
+// information about the private key that is used for signing. Callers must
+// be aware that the given hash cannot be chosen by an adversery. Common
+// solution is to hash any input before calculating the signature.
+//
+// The produced signature is in the [R || S || V] format where V is 0 or 1.
+func (s *SignerSecp256k1Go) Sign(privKey PrivateKey, msg []byte) Signature {
+	prv, _ := ToECDSA(privKey.Bytes)
+	hash := Sha256(msg)
+	if len(hash) != 32 {
+		panic(fmt.Errorf("hash is required to be exactly 32 bytes (%d)", len(hash)))
+	}
+	if prv.Curve != btcec.S256() {
+		panic(fmt.Errorf("private key curve is not secp256k1"))
+	}
+	sig, err := btcec.SignCompact(btcec.S256(), (*btcec.PrivateKey)(prv), hash, false)
+	if err != nil {
+		panic(err)
+	}
+	// Convert to Ethereum signature format with 'recovery id' v at the end.
+	v := sig[0] - 27
+	copy(sig, sig[1:])
+	sig[64] = v
+	return SignatureFromBytes(s.GetCryptoType(), sig)
+}
+
+// VerifySignature checks that the given public key created signature over hash.
+// The public key should be in compressed (33 bytes) or uncompressed (65 bytes) format.
+// The signature should have the 64 byte [R || S] format.
+func (s *SignerSecp256k1Go) Verify(pubKey PublicKey, signature Signature, msg []byte) bool {
+	hash := Sha256(msg)
+	signature = s.DealRecoverID(signature)
+	sigs := signature.Bytes
+	sig := &btcec.Signature{R: new(big.Int).SetBytes(sigs[:32]), S: new(big.Int).SetBytes(sigs[32:])}
+	key, err := btcec.ParsePubKey(pubKey.Bytes, btcec.S256())
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+	// Reject malleable signatures. libsecp256k1 does this check but btcec doesn't.
+	if sig.S.Cmp(secp256k1halfN) > 0 {
+		fmt.Println("sec")
+		return false
+	}
+	return sig.Verify(hash, key)
+}
+
+func (s *SignerSecp256k1Go) DealRecoverID(sig Signature) Signature {
+	l := len(sig.Bytes)
+	if l == sigLength+1 {
+		sig.Bytes = sig.Bytes[:l-1]
+	}
+	return sig
+}
+
+func (s *SignerSecp256k1Go) PubKey(privKey PrivateKey) PublicKey {
+	_, ecdsapub := btcec.PrivKeyFromBytes(btcec.S256(), privKey.Bytes)
+	pub := FromECDSAPub((*ecdsa.PublicKey)(ecdsapub))
+	return PublicKeyFromBytes(CryptoTypeSecp256k1, pub[:])
+}
 
 func TestSignerNewPrivKeyGO(t *testing.T) {
 	t.Parallel()
@@ -38,7 +108,7 @@ func TestSignerNewPrivKeyGO(t *testing.T) {
 	}
 	fmt.Println(hex.EncodeToString(sig.Bytes))
 
-	signer2:= SignerSecp256k1Go{}
+	signer2 := SignerSecp256k1Go{}
 	sig2 := signer2.Sign(priv, b)
 	fmt.Println(hex.EncodeToString(sig2.Bytes))
 	if !signer2.Verify(pk, sig, b) {
@@ -50,21 +120,21 @@ func TestSignerNewPrivKeyGO(t *testing.T) {
 func TestSignBenchMarks(t *testing.T) {
 	signer := SignerSecp256k1{}
 	pk, priv := signer.RandomKeyPair()
-	signer2:= SignerSecp256k1Go{}
+	signer2 := SignerSecp256k1Go{}
 	var txs1 types.Txis
-	var txs2  types.Txis
+	var txs2 types.Txis
 	N := 10000
 
-	for i:=0;i<N;i++ {
-		txs1 = append(txs1,types.RandomTx())
+	for i := 0; i < N; i++ {
+		txs1 = append(txs1, types.RandomTx())
 	}
-	for i:=0;i<N;i++ {
-		txs2 = append(txs2,types.RandomTx())
+	for i := 0; i < N; i++ {
+		txs2 = append(txs2, types.RandomTx())
 	}
 	fmt.Println("started")
 	start := time.Now()
-	for i:=0;i<len(txs1);i++ {
-		b:=txs1[i].SignatureTargets()
+	for i := 0; i < len(txs1); i++ {
+		b := txs1[i].SignatureTargets()
 		sig := signer.Sign(priv, b)
 		txs1[i].GetBase().Signature = sig.Bytes
 		//if !signer.Verify(pk, sig, b) {
@@ -73,8 +143,8 @@ func TestSignBenchMarks(t *testing.T) {
 	}
 	fmt.Println("secp cgo used for signing ", time.Since(start))
 	start = time.Now()
-	for i:=0;i<len(txs1);i++ {
-		b:=txs1[i].SignatureTargets()
+	for i := 0; i < len(txs1); i++ {
+		b := txs1[i].SignatureTargets()
 		sig := signer2.Sign(priv, b)
 		txs2[i].GetBase().Signature = sig.Bytes
 		//if !signer2.Verify(pk, sig, b) {
@@ -83,8 +153,8 @@ func TestSignBenchMarks(t *testing.T) {
 	}
 	fmt.Println("secp go used for signing ", time.Since(start))
 	start = time.Now()
-	for i:=0;i<len(txs2);i++ {
-		b:=txs2[i].SignatureTargets()
+	for i := 0; i < len(txs2); i++ {
+		b := txs2[i].SignatureTargets()
 		sig := signer2.Sign(priv, b)
 		txs2[i].GetBase().Signature = sig.Bytes
 		//if !signer2.Verify(pk, sig, b) {
@@ -93,8 +163,8 @@ func TestSignBenchMarks(t *testing.T) {
 	}
 	fmt.Println("secp go used for signing ", time.Since(start))
 	start = time.Now()
-	for i:=0;i<len(txs2);i++ {
-		b:=txs2[i].SignatureTargets()
+	for i := 0; i < len(txs2); i++ {
+		b := txs2[i].SignatureTargets()
 		sig := signer.Sign(priv, b)
 		txs2[i].GetBase().Signature = sig.Bytes
 		//if !signer.Verify(pk, sig, b) {
@@ -103,7 +173,7 @@ func TestSignBenchMarks(t *testing.T) {
 	}
 	fmt.Println("secp cgo used for signing ", time.Since(start))
 	start = time.Now()
-	for i:=0;i<len(txs1);i++ {
+	for i := 0; i < len(txs1); i++ {
 		b := txs1[i].SignatureTargets()
 		sig := txs1[i].GetBase().Signature
 		if !signer2.Verify(pk, SignatureFromBytes(CryptoTypeSecp256k1, sig), b) {
@@ -112,7 +182,7 @@ func TestSignBenchMarks(t *testing.T) {
 	}
 	fmt.Println("secp go used for verifying ", time.Since(start))
 	start = time.Now()
-	for i:=0;i<len(txs1);i++ {
+	for i := 0; i < len(txs1); i++ {
 		b := txs1[i].SignatureTargets()
 		sig := txs1[i].GetBase().Signature
 		if !signer.Verify(pk, SignatureFromBytes(CryptoTypeSecp256k1, sig), b) {
@@ -121,7 +191,7 @@ func TestSignBenchMarks(t *testing.T) {
 	}
 	fmt.Println("secp cgo used for verifying ", time.Since(start))
 	start = time.Now()
-	for i:=0;i<len(txs1);i++ {
+	for i := 0; i < len(txs1); i++ {
 		b := txs2[i].SignatureTargets()
 		sig := txs2[i].GetBase().Signature
 		if !signer.Verify(pk, SignatureFromBytes(CryptoTypeSecp256k1, sig), b) {
@@ -130,7 +200,7 @@ func TestSignBenchMarks(t *testing.T) {
 	}
 	fmt.Println("secp cgo used for verifying ", time.Since(start))
 	start = time.Now()
-	for i:=0;i<len(txs1);i++ {
+	for i := 0; i < len(txs1); i++ {
 		b := txs1[i].SignatureTargets()
 		sig := txs1[i].GetBase().Signature
 		if !signer2.Verify(pk, SignatureFromBytes(CryptoTypeSecp256k1, sig), b) {

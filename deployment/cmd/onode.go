1475c4e149b620eb11ce537bda65b50dbb4cb596
Uni <boh_zhu@163.com>
2019-06-05 17:35:58 +0800

    update deployment tool

--- deployment/cmd/onode.go
+++ deployment/cmd/onode.go
@@ -1,1 +1,37 @@
 package cmd
+
+import (
+	"encoding/hex"
+	"fmt"
+	"github.com/annchain/OG/common/crypto"
+	"github.com/annchain/OG/p2p/onode"
+	"github.com/spf13/cobra"
+	"net"
+)
+
+var (
+	onodeCmd = &cobra.Command{
+		Use: "onode",
+		Short: "onode helps you generate a random onode address",
+		Run: genOnode,
+	}
+	output string
+)
+
+func onodeInit() {
+	onodeCmd.Flags().StringVarP(&output, "output", "o", "", "")
+}
+
+func genOnode(cmd *cobra.Command, args []string) {
+	var bootUrl = "47.100.222.11"
+
+	key, err := crypto.GenerateKey()
+	if err != nil {
+		panic(fmt.Sprintf("failed to generate ephemeral node key: %v", err))
+	}
+	nodekey := hex.EncodeToString(crypto.FromECDSA(key))
+	node := onode.NewV4(&key.PublicKey, net.ParseIP(bootUrl), 8001, 8001)
+
+	fmt.Println("nodekey: ", nodekey)
+	fmt.Println("ndoe: ", node.String())
+}
--- deployment/cmd/root.go
+++ deployment/cmd/root.go
@@ -1,11 +1,29 @@
 package cmd
 
 import (
-
+	"fmt"
 	"github.com/spf13/cobra"
+	"os"
 )
 
 var rootCmd = &cobra.Command{
 	Use: "OG tool for deployment",
-	Short: "OG ",
+	Short: "deploy helps you to create boot node address and init genesis consensus public keys",
+}
+
+func init() {
+	onodeInit()
+	rootCmd.AddCommand(onodeCmd)
+
+	genInit()
+	genCmd.AddCommand(genCmd)
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }

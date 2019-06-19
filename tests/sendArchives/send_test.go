cdbecf28771a07b873f40bcef7792cdf1c516d5e
metabd <met_a@foxmail.com>
2019-06-19 10:42:21 +0800

    add missing licence

--- tests/sendArchives/send_test.go
+++ tests/sendArchives/send_test.go
@@ -1,3 +1,16 @@
+// Copyright © 2019 Annchain Authors <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package sendArchives
 
 import (
@@ -9,7 +22,7 @@ import (
 )
 
 type TxReq struct {
-	Data []byte `json:"data"`
+	Data string `json:"data"`
 }
 
 func TestArchives(t *testing.T) {
@@ -31,7 +44,7 @@ func TestArchives(t *testing.T) {
 			req := httplib.Post(Host + "/new_archive")
 
 			txReq := TxReq{
-				Data: data,
+				Data: string(data),
 			}
 			_, err := req.JSONBody(&txReq)
 			if err != nil {

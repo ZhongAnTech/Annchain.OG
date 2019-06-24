3b58dd4a7e5ba1dd89c7fce087f69a458856b332
metabd <met_a@foxmail.com>
2019-06-24 20:24:03 +0800

    add archive test

--- tests/testSendArchives/sendArchives_test.go
+++ tests/testSendArchives/sendArchives_test.go
@@ -1,45 +1,77 @@
 package testSendArchives
 
 import (
+	"bytes"
 	"fmt"
-	"github.com/annchain/OG/client/httplib"
 	"github.com/annchain/OG/types"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
 	"testing"
 	"time"
 )
 
 func TestSend(t *testing.T){
 	var ar [][]byte
-   for i:=0;i<10;i++ {
+   for i:=0;i<100000;i++ {
       ar = append(ar,types.RandomHash().ToBytes())
    }
+   a:= app{
+   	client: &http.Client{
+   		Timeout:time.Second*10,
+	},
+   }
    for i,v := range ar {
-   	 go sendArchiveData(v,i)
+   	 go a.sendArchiveData(v,i)
    }
    time.Sleep(time.Second*100)
 	return
 }
-var ulr = "http://172.28.152.101:8000/new_archive"
+
+type app struct {
+	client *http.Client
+}
+
+var url = "http://172.28.152.101:8000/new_archive"
 
 type txRequest struct {
 	Data []byte `json:"data"`
 }
 
-func sendArchiveData (data[]byte,i int ) {
-	req := httplib.NewBeegoRequest(ulr,"POST")
-	req.SetTimeout(time.Second*10,time.Second*10)
+func (a *app)sendArchiveData (data[]byte,i int ) {
+	//req := httplib.NewBeegoRequest(url,"POST")
+	//req.SetTimeout(time.Second*10,time.Second*10)
 	tx:= txRequest{
 		Data:data,
 	}
-	req,err := req.JSONBody(&tx)
+	data,err := json.Marshal(&tx)
 	if err!=nil {
 		panic(err)
 	}
+	r := bytes.NewReader(data)
+	c:= http.DefaultClient
+	c.Timeout = 10*time.Second
+	resp,err := c.Post(url,"application/json",r)
+	if err!=nil {
+		fmt.Println(err)
+		return
+	}
 	now := time.Now()
-	str,err:=req.String()
+	defer resp.Body.Close()
+	resDate ,err := ioutil.ReadAll(resp.Body)
+	if err!=nil {
+		panic(err)
+	}
+
+	str:=string(resDate)
 	if err!=nil {
 		fmt.Println(i,str,err)
 	}
-	fmt.Println(i, err, time.Since(now))
+	if resp.StatusCode!=200 {
+		//panic( resp.StatusCode)
+		fmt.Println(resp.StatusCode)
+		return
+	}
+	fmt.Println(i, err, time.Since(now), str)
 
 }

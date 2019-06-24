d1ea3b66f0714ab42089b42dfab52b8adeab5d38
jenson <metab@foxmail.com>
2019-06-24 15:16:24 +0800

    rpc : test archive sending profile

---
+++ tests/testSendArchives/sendArchives_test.go
@@ -0,0 +1,45 @@
+package testSendArchives
+
+import (
+	"fmt"
+	"github.com/annchain/OG/client/httplib"
+	"github.com/annchain/OG/types"
+	"testing"
+	"time"
+)
+
+func TestSend(t *testing.T){
+	var ar [][]byte
+   for i:=0;i<1000;i++ {
+      ar = append(ar,types.RandomHash().ToBytes())
+   }
+   for i,v := range ar {
+   	 go sendArchiveData(v,i)
+   }
+   time.Sleep(time.Second*100)
+	return
+}
+var ulr = "http://172.28.152.101:8000/new_archive"
+
+type txRequest struct {
+	Data []byte `json:"data"`
+}
+
+func sendArchiveData (data[]byte,i int ) {
+	req := httplib.NewBeegoRequest(ulr,"POST")
+	req.SetTimeout(time.Second*10,time.Second*10)
+	tx:= txRequest{
+		Data:data,
+	}
+	req,err := req.JSONBody(&tx)
+	if err!=nil {
+		panic(err)
+	}
+	now := time.Now()
+	str,err:=req.String()
+	if err!=nil {
+		fmt.Println(i,str,err)
+	}
+	fmt.Println(i, err, time.Since(now))
+
+}

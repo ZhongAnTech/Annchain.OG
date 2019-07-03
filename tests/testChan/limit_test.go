5e5c3f42d02153368f3bf0369293857a0588d9da
metabd <met_a@foxmail.com>
2019-07-03 10:52:17 +0800

    add  db concurency limit

---
+++ tests/testChan/limit_test.go
@@ -0,0 +1,59 @@
+package testChan
+
+import (
+	"fmt"
+	"runtime"
+	"testing"
+	"time"
+)
+
+func TestLimit(t *testing.T) {
+	a := app{
+		ch: make(chan  int, 3),
+		quit: make(chan  bool),
+	}
+	//go a.loop()
+
+	for i:=0;i<10;i++ {
+		fmt.Println("before write  ",i, time.Now())
+	       a.process(i)
+		fmt.Println("after write ",i,time.Now())
+	}
+	fmt.Println(runtime.NumGoroutine())
+
+}
+
+
+type app struct {
+ ch chan int
+ quit chan bool
+}
+
+func (a *app)loop (){
+
+	for {
+		select {
+		 case i:= <-a.ch:
+		  fmt.Println("start " ,i, time.Now())
+		 time.Sleep(time.Millisecond*500)
+			 fmt.Println("stop " ,i,time.Now())
+		 case <-a.quit:
+		 	fmt.Println("exit")
+			 return
+
+		}
+	}
+}
+
+func (a *app)process(i int  ) {
+	a.ch<-i
+	go func() {
+		fmt.Println("start " ,i, time.Now())
+		time.Sleep(time.Millisecond*500)
+		fmt.Println("stop " ,i,time.Now())
+		<-a.ch
+	}()
+
+}
+
+

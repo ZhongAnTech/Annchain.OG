fa31a14ae2c310bd35fc1704777f20a0fc53502b
jenson <metab@foxmail.com>
2019-07-30 20:07:05 +0800

    signatrue verification can be disabled

--- node/node.go
+++ node/node.go
@@ -179,6 +179,7 @@ func NewNode() *Node {
 		txFormatVerifier.NoVerifyMaxTxHash = true
 		logrus.Info("no verify max tx hash")
 	}
+	txFormatVerifier.NoVerifySignatrue =viper.GetBool("tx_buffer.no_verify_signature")
 	//verify format first , set address and then verify graph
 	verifiers := []og.Verifier{txFormatVerifier, graphVerifier}
 

8e7083357a3030d4116c4f7c60848a413e80e62f
Uni <boh_zhu@163.com>
2019-11-15 15:12:13 +0800

    Update server.go

--- wserver/server.go
+++ wserver/server.go
@@ -170,7 +170,7 @@ func (s *Server) Name() string {
 func (s *Server) WatchNewTxs() {
 	ticker := time.NewTicker(time.Millisecond * 300)
 	defer ticker.Stop()
-	var uidata *UIData
+	//var uidata *UIData
 	var blockdbData *BlockDbUIData
 	for {
 		select {

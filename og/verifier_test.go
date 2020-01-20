aaae808688dad9792dc59074ea843af6cd96a772
Uni <boh_zhu@163.com>
2020-01-20 18:23:19 +0800

    test secp256k1 recover id signing

--- og/verifier_test.go
+++ og/verifier_test.go
@@ -17,12 +17,12 @@ import (
 	"fmt"
 	"github.com/annchain/OG/common"
 	"github.com/annchain/OG/common/crypto"
+	"github.com/annchain/OG/common/math"
 	"github.com/annchain/OG/types"
 	"github.com/annchain/OG/types/tx_types"
 	"github.com/magiconair/properties/assert"
 	"github.com/sirupsen/logrus"
 	"testing"
-	"time"
 )
 
 func buildTx(from common.Address, accountNonce uint64) *tx_types.Tx {
@@ -197,39 +197,72 @@ func TestConsensusVerifier_Verify(t *testing.T) {
 
 func TestVerify(t *testing.T) {
 	signer := crypto.NewSigner(crypto.CryptoTypeSecp256k1)
-	pub, priv := signer.RandomKeyPair()
-	var txis types.Txis
-	//var sigTerGets [][]byte
-	addr := pub.Address()
-	for i := 0; i < 10000; i++ {
-		tx := tx_types.RandomTx()
-		tx.From = &addr
-		tx.Signature = signer.Sign(priv, tx.SignatureTargets()).Bytes
-		tx.PublicKey = pub.Bytes
-		txis = append(txis, tx)
-	}
-	v := TxFormatVerifier{NoVerifyMindHash: true, NoVerifyMaxTxHash: true}
-	now := time.Now()
-	fmt.Println("start ", now)
-	for i, tx := range txis {
-		ok := v.VerifySignature(tx)
-		if !ok {
-			t.Fatal(ok, tx, i)
-		}
-	}
-	fmt.Println("used ", time.Since(now))
-	start := time.Now()
-	newSginer := &TestSigner{}
-	crypto.Signer = newSginer
-	fmt.Println(crypto.Signer.CanRecoverPubFromSig())
-	for i, tx := range txis {
-		ok := v.VerifySignature(tx)
-		if !ok {
-			t.Fatal(ok, tx, i)
-		}
-	}
-	fmt.Println("used ", time.Since(start))
+	//_, priv := signer.RandomKeyPair()
 
+	//pub, priv := signer.RandomKeyPair()
+	//var txis types.Txis
+	////var sigTerGets [][]byte
+	//addr := pub.Address()
+	//for i := 0; i < 10000; i++ {
+	//	tx := tx_types.RandomTx()
+	//	tx.From = &addr
+	//	tx.Signature = signer.Sign(priv, tx.SignatureTargets()).Bytes
+	//	tx.PublicKey = pub.Bytes
+	//	txis = append(txis, tx)
+	//}
+	v := TxFormatVerifier{NoVerifyMindHash: true, NoVerifyMaxTxHash: true}
+	//now := time.Now()
+	//fmt.Println("start ", now)
+	//for i, tx := range txis {
+	//	ok := v.VerifySignature(tx)
+	//	if !ok {
+	//		t.Fatal(ok, tx, i)
+	//	}
+	//}
+	//fmt.Println("used ", time.Since(now))
+	//start := time.Now()
+	newSigner := &TestSigner{}
+	crypto.Signer = newSigner
+	//fmt.Println(crypto.Signer.CanRecoverPubFromSig())
+	//for i, tx := range txis {
+	//	ok := v.VerifySignature(tx)
+	//	if !ok {
+	//		t.Fatal(ok, tx, i)
+	//	}
+	//}
+	//fmt.Println("used ", time.Since(start))
+
+	// test signature with recover id
+	pub, priv := signer.RandomKeyPair()
+	addr := signer.AddressFromPubKeyBytes(pub.Bytes)
+
+	tx := tx_types.RandomTx()
+	tx.From = &addr
+	tx.To = common.HexToAddress("0xc1eb507017610d2134bc70146731d777a25c2889")
+	tx.AccountNonce = 17320
+	tx.Value, _ = math.NewBigIntFromString("894385949183117216", 10)
+	tx.Data = common.Hex2Bytes("")
+	tx.TokenId = 0
+
+	sig := signer.Sign(priv, tx.SignatureTargets())
+	tx.Signature = sig.Bytes
+	//tx.Signature = common.Hex2Bytes("b08351552c718995e0d433850094e4e17eb2977bb479cae6488f6958c865c147313bc114142ae40ec90928466821a690b543b71489abea13cb6a8959e741fc211c")
+	//tx.PublicKey = pub.Bytes
+
+	fmt.Println("from: \t\t", tx.From.Hex())
+	fmt.Println("to: \t\t", tx.To.Hex())
+	fmt.Println("nonce: \t\t", tx.AccountNonce)
+	fmt.Println("value: \t\t", tx.Value.String())
+	fmt.Println(fmt.Sprintf("data: \t\t%x", tx.Data))
+	fmt.Println("tokenID: \t", tx.TokenId)
+	fmt.Println(fmt.Sprintf("sig: \t\t%x", []byte(tx.Signature)))
+	fmt.Println(fmt.Sprintf("priv: \t\t%x", priv.Bytes))
+
+	fmt.Println(fmt.Sprintf("sig targets: \t%x", tx.SignatureTargets()))
+
+	if !v.VerifySignature(tx) {
+		t.Fatalf("signature not correct")
+	}
 }
 
 type TestSigner struct {

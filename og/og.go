3f8b3591dfb41c6c59e6ddc5e91fd93aa2fa7b05
Alchemyhx <alchemyhx97@icloud.com>
2020-06-11 12:14:00 +0800

    update fields about PushNodeData() and soccerdash report address

--- og/og.go
+++ og/og.go
@@ -198,18 +198,18 @@ func (og *Og) GetSequencerByHash(hash common.Hash) *tx_types.Sequencer {
 func (og *Og) BroadcastLatestSequencer() {
 	var notSend bool
 	var mu sync.RWMutex
-	var enableReport = viper.GetBool("report.enable")
-	var reportKey string
-	if v, ok := os.LookupEnv("HOSTNAME"); ok {
-		reportKey = v
-	} else {
-		reportKey = "node_" + fmt.Sprintf("%d", viper.GetInt("debug.node_id"))
-	}
-	r := &soccerdash.Reporter{
-		Id:            reportKey,
-		TargetAddress: viper.GetString("report.address"),
-	}
-	var reportTime = time.Now()
+	// var enableReport = viper.GetBool("report.enable")
+	// var reportKey string
+	// if v, ok := os.LookupEnv("HOSTNAME"); ok {
+	// 	reportKey = v
+	// } else {
+	// 	reportKey = "node_" + fmt.Sprintf("%d", viper.GetInt("debug.node_id"))
+	// }
+	// r := &soccerdash.Reporter{
+	// 	Id:            reportKey,
+	// 	TargetAddress: viper.GetString("report.address"),
+	// }
+	// var reportTime = time.Now()
 	for {
 		select {
 		case <-og.NewLatestSequencerCh:
@@ -234,17 +234,17 @@ func (og *Og) BroadcastLatestSequencer() {
 				og.Manager.BroadcastMessage(p2p_message.MessageTypeSequencerHeader, &msg)
 			}
 			goroutine.New(function)
-			if enableReport {
-				re := func() {
-					r.Report("height", seq.GetHeight(), false)
-					r.Report("hash", seq.GetTxHash().Hex(), false)
-					r.Report("from", seq.GetSender().Hex(), false)
-					r.Report("nonce", seq.GetNonce(), false)
-					r.Report("root", seq.StateRoot.Hex(), false)
-				}
-				goroutine.New(re)
-				reportTime = time.Now()
-			}
+			// if enableReport {
+			// 	re := func() {
+			// 		r.Report("height", seq.GetHeight(), false)
+			// 		r.Report("hash", seq.GetTxHash().Hex(), false)
+			// 		r.Report("from", seq.GetSender().Hex(), false)
+			// 		r.Report("nonce", seq.GetNonce(), false)
+			// 		r.Report("root", seq.StateRoot.Hex(), false)
+			// 	}
+			// 	goroutine.New(re)
+			// 	reportTime = time.Now()
+			// }
 		case <-time.After(200 * time.Millisecond):
 			if notSend && !og.Manager.Hub.Downloader.Synchronising() {
 				mu.Lock()
@@ -261,14 +261,14 @@ func (og *Og) BroadcastLatestSequencer() {
 				}
 				goroutine.New(function)
 			}
-			if enableReport && time.Now().Sub(reportTime) > time.Second*15 {
-				re := func() {
-					r.Report("msg", "synchronising or stopping?", false)
-					r.Report("height", og.Dag.LatestSequencer().Height, false)
-				}
-				goroutine.New(re)
-				reportTime = time.Now()
-			}
+			// if enableReport && time.Now().Sub(reportTime) > time.Second*15 {
+			// 	re := func() {
+			// 		r.Report("msg", "synchronising or stopping?", false)
+			// 		r.Report("height", og.Dag.LatestSequencer().Height, false)
+			// 	}
+			// 	goroutine.New(re)
+			// 	reportTime = time.Now()
+			// }
 		case <-og.quit:
 			logrus.Info("hub BroadcastLatestSequencer received quit message. Quitting...")
 			return
@@ -280,40 +280,62 @@ func (og *Og) BroadcastLatestSequencer() {
 // It uses goCron as time scheduler.
 func (og *Og) PushNodeData() {
 	// var enableReport = viper.GetBool("report.enable")
-	// var reportKey string
-	// if v, ok := os.LookupEnv("HOSTNAME"); ok {
-	// 	reportKey = v
-	// } else {
-	// 	reportKey = "node_" + fmt.Sprintf("%d", viper.GetInt("debug.node_id"))
-	// }
+	var reportKey string
+	if v, ok := os.LookupEnv("HOSTNAME"); ok {
+		reportKey = v
+	} else {
+		reportKey = "node_" + fmt.Sprintf("%d", viper.GetInt("debug.node_id"))
+	}
 
-	// r := &soccerdash.Reporter{
-	// 	Id:            reportKey,
-	// 	TargetAddress: viper.GetString("report.address"),
-	// }
-	r := soccerdash.Reporter{
-		Id:            "C1",
-		TargetAddress: "127.0.0.1:8080",
+	r := &soccerdash.Reporter{
+		Id:            reportKey,
+		TargetAddress: viper.GetString("report.address"),
 	}
 
 	s := gocron.NewScheduler(time.UTC)
 	s.Every(2).Seconds().Do(func() {
-		goroutine.New(func() {
-			r.Report("TxPoolNum", og.TxPool.GetTxNum(), false)
-		})
+		r.Report("TxPoolNum", og.TxPool.GetTxNum(), false)
 	})
 
 	s.Every(10).Seconds().Do(func() {
-		goroutine.New(func() {
-			r.Report("ConnNum", "10", false)
-		})
+		r.Report("ConnNum", "10", false)
+		r.Report("IsProducer", false, false)
+	})
+
+	s.Every(30).Seconds().Do(func() {
+		r.Report("NodeDelay", "30", false)
 	})
 
 	s.Every(1).Minute().Do(func() {
-		goroutine.New(func() {
-			r.Report("Version", "1.0", false)
-			r.Report("LatestNode", "123", false)
-		})
+		r.Report("Version", "1.0", false)
+		r.Report("NodeName", "123", false)
+	})
+
+	s.Every(1).Minute().Do(func() {
+		SeqMsg := `{
+			data: {
+			Type: 1,
+			Hash: "0x48e44af30be97556e377b58de3e5d77e6801a82de906e647e121fe1497effac2",
+			ParentsHash: [
+			"0x0b82bab454743a0f0cae194b15c8bfa049cfc4fa230b1bb0fd42e4790ba39a69"
+			],
+			AccountNonce: 2157,
+			Height: 2157,
+			PublicKey: "BIDG6ARHwZspJ1gUJTuHrjre0pyL7YJvG7E+UnbujaOVym+AjVi6jZIhptr92lwqi0xkXyXnI10gHt8RtnRHzrI=",
+			Signature: "0x823d16432ba343f3a79d04ebadbab1683a5b93074d84e3c367c4815e568c682b156bff6c1b0a574319651dbe2d3e21f3ce21b5ea3085ea7de290371fb4b1297b01",
+			MineNonce: 0,
+			Weight: 2157,
+			Version: 0,
+			Issuer: "0x7349f7a6f622378d5fb0e2c16b9d4a3e5237c187",
+			BlsJointSig: "0x",
+			BlsJointPubKey: "0x",
+			StateRoot: "0x980e8a14f85aaa1dfc7a4377f2d7ef7feaada75b08ab8b8e73590304bee548dc",
+			Proposing: false,
+			Timestamp: 1591769404219
+			},
+			err: ""
+		}`
+		r.Report("LatestSequencer", SeqMsg, false)
 	})
 
 	s.StartBlocking()

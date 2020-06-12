daf15fb20af1be67fec422ae38589facdffda33c
Gan Zejun <ganzejun@zhongan.io>
2020-06-12 20:22:12 +0800

    account generation tbc

--- arefactor/og/types/account.go
+++ arefactor/og/types/account.go
@@ -1,1 +1,13 @@
 package types
+
+import (
+	"github.com/annchain/OG/arefactor/og_interface"
+	"github.com/annchain/OG/arefactor/ogcrypto_interface"
+)
+
+// OgAccount represents a full account of a user.
+type OgAccount struct {
+	PublicKey  ogcrypto_interface.PublicKey
+	PrivateKey ogcrypto_interface.PrivateKey
+	Address    og_interface.Address
+}
